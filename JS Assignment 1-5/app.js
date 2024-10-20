6cd0a01bee7ed240a4c09639f3518b6fe5b45155
IZHAN QURESHI <izhanqureshi07@gmail.com>
2024-10-20 04:10:09 -0700

    Add files via upload

---
+++ JS Assignment 1-5/app.js
@@ -0,0 +1,329 @@
+                      // ASSIGNMENT NO : 1
+
+// QUESTION NO : 1
+alert("ASSLAM U ALAIKUM Everyone");
+
+// QUESTION NO : 2
+
+alert("Error! Please enter a valid password");
+
+// QUESTION NO : 3
+
+alert("Welcome to js land.... \n Happy Coding!");
+
+// QUESTION NO : 4
+
+alert("Welcome to Js land...");
+alert("Happy Coding!");
+
+// QUESTION NO : 5
+var msg= "Hello...i can run js through my web brower's console"
+ alert(msg);
+document.write(msg);
+
+
+
+
+                                      //  ASSIGNMENT NO : 2
+
+
+
+// QUESTION NO : 1
+
+var username;
+
+// QUESTION NO : 2
+
+var myName = "Raja Shahib";
+
+// QUESTION NO : 3
+
+var message;
+message = "Hello PAAJI";
+alert(message);
+
+// QUESTION NO : 4
+
+var name1 = "Jhone Doe";
+var age = "15 year old";
+var degree=  "Certified Mobile Application Development";
+alert(name1);
+alert(age);
+alert(degree);
+
+
+
+// QUESTION NO : 6
+
+var email = "example@gmail.com";
+alert("My email address is " + email);
+
+
+// Question NO : 7 
+
+var book = "A smarter way to learn javascript";
+alert("I am trying to learn from the book  " + book);
+
+
+// Question NO : 8 
+
+var text = "Yah! I  can write HTML content through Javascript";
+console.log(text);
+
+// Question NO : 9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+                                            //  ASSIGNMENT NO : 3
+
+// Question NO : 1
+
+let newAge = 15;
+alert("I am " + newAge   +" year old")
+
+// Question NO : 2
+
+let num = 14;
+alert("You have visited this site " + num + "times");
+
+
+
+// Question NO : 3
+
+let birthYear = 1990;
+document.write("<br/> My birth year is "  + birthYear + "  \n MY data type of this  variable is number" );
+
+
+// Question NO : 4
+
+var visitorName = "jhone Doe";
+var productTitle = "T-shirt";
+var Quantity = 7;
+
+document.write("<br/>",visitorName + " ordered " + Quantity + "" + productTitle + " (S) on XYZ clothing store ");
+
+
+                                            // Assignment NO : 5
+
+
+// Question No : 1
+
+
+var x,y,z;
+
+// Question No : 2
+
+
+// legal 
+var Names;
+var Names_for_name;
+var $Names;
+var _Names;
+var Name123;
+
+// illegal
+
+// var 123ahmed;
+// var ahme+d;
+// var ,ali;
+// var ahmed ali;
+// var  alert;
+
+// Question No : 3
+
+document.write("<br/> Rules for naming javascript");
+document.write("<br/> Variables name can only contain number,$ and - ");
+document.write("<br/> Variables must begin with a letter,$ or - For example $name, _name or name");
+document.write("<br/> Variable names are case  case sensative");
+document.write("<br/> Variable names should not be JS  keywords");
+
+
+
+
+
+                                // Assignment NO : 5
+                                
+// Question NO : 1              
+
+
+let a = 3;
+let b = 5;
+let add = a + b;
+alert("sum of " + a + " and " + b + " is " + add );
+
+
+
+// Question NO : 2
+
+
+let c = 3;
+let d = 5;
+let subtract = c - d;
+alert("Subtract of " + c + " and " + d + " is " + subtract );
+
+
+let e = 3;
+let f = 5;
+let mul = e * f;
+alert("Multiply of " + e + " and " + f + " is " + mul );
+
+
+let g = 3;
+let h = 5;
+let divi = g / h;
+alert("Division  of " + g + " and  " + h + " is " + divi );
+
+
+let i = 3;
+let  j= 5;
+let mod= i % j;
+alert("Reminder  of " + i + " and " + j + " is " + mod );
+
+
+
+// Question NO : 3
+
+
+var  num1;
+document.write( " <br/> Value after variable declaration is: "  + num1 ); 
+ num1 = 21;
+ document.write( " <br/> Intial value : " + num1);
+ num1++;
+ document.write( "<br/> Value after increment : " + num1);
+num1 = num1 + 7;
+document.write( " <br/> Value after adding  7 is : "  + num1);
+num1--;
+document.write( "<br/> Value after decrement is : " + num1 );
+num1 = num1 % 2;
+document.write( " <br/> The reminder is : " + num1);
+
+
+// Question NO : 4
+
+
+var ticket = 600;
+ticket = ticket * 5 ;
+document.write("<br/>Total cost of 5 ticket to a movie is :" + ticket);
+
+// Question no : 5
+
+
+document.write(" Table of 4");
+var num2 = 4;
+var num3 = 1;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>", num2 + " x " + num3 + " = "  + num4)
+num3++;
+var num4 = num2 * num3;
+document.write("<br/>",num2 + " x " + num3 + " = "  + num4);
+
+
+
+// Question No : 6
+
+var cel = 50;
+var far = ( cel * 9/5 ) + 35;
+document.write("<br/>",cel +" C and " + far + "C");
+cel = (far-32) * 5/9;
+document.write(  "<br/>",far +" C and " + cel + "C");
+
+
+
+// Question no : 7
+
+ var item1 = 650;
+ document.write("<br/> Price of item 1 is : " + item1 );
+ var Quantity_item1 = 3;
+ document.write(" <br/> Quantity of item 1 is : " + Quantity_item1 );
+  var item2 = 100;
+  document.write("<br/> Price of item 2 is : " + item2 );
+ var Quantity_item2 = 7;
+ document.write("<br/> Quantity of item 2  is : " + Quantity_item2 );
+  var charges = 100;
+  document.write("<br/> Shipping Charges : " + charges );
+
+  var  total_cost = (item1 * Quantity_item1) + (Quantity_item2 * item2) + charges;
+  document.write(" <br/> Total cost of your order is : " + total_cost );
+
+
+//   Question No : 8
+
+
+var total_marks = 980;
+var marks_obtained = 804;
+var percentage = (marks_obtained/total_marks) * 100;
+document.write("<br/> Total marks : " + total_marks);
+document.write(" <br/> Marks obtained : " + marks_obtained);
+document.write("<br/> Percentage : " + percentage  + "%");
+
+// Question NO : 9
+
+
+var  dollar = 10;
+var  riyal = 25;
+var rupees  = (dollar*104.80) + (riyal*28);
+
+document.write("<br/> Total currency in PKR :" + rupees);
+
+
+// Question NO : 10
+
+
+var number1 =  7;
+number1= + 5;
+number1 = number1 * 10;
+var result = number1/2;
+console.log(result);
+
+// Question NO : 11
+
+var current_year = 2024;
+var birth_year = 2004;
+var your_age = 2024-2004;
+document.write("<br/> Current year is :  " + current_year ,) ; 
+document.write( " <br/> birth year is : "+ birth_year);
+document.write(" <br/> Your age is :" + your_age);
+
+
+// Question NO : 12
+
+
+var rad =  2.5;
+document.write("<br/> Your radius is :" + rad);
+var circum = 2*3.142*rad;
+document.write (" <br/> Circumference  of a circle is  : " + circum);
+var area = 3.142*rad*rad;
+document.write ("<br/> area of a circle  is  :" + area);
+
+// Question NO : 13;
+
+var snack = " chocolate chips ";
+var currentAge = 15;
+var maximumAge = 65;
+var snackPerDay = 3;
+var totalEat = (maximumAge*snackPerDay)-(currentAge*snackPerDay)
+document.write  (" <br/> You will need" + totalEat + snack + " to last  you");  
